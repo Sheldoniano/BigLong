7bce8129869d5b59f198fb05b27375edb7b1e747
Juan <juan16alfaro@gmail.com>
2020-03-02 08:23:23 -0500

    pruebas con bug

--- BigLong/BigLong.cpp
+++ BigLong/BigLong.cpp
@@ -1,6 +1,5 @@
 // BigLong.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
-#include "pch.h"
 #include <iostream>
 #include <conio.h>
 #include <iomanip>  // std::setfill, std::setw
