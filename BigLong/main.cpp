9c2f9c5452a68ba2e2f2d69f8bae1d34a0f948f9
Juan <juan16alfaro@gmail.com>
2020-03-28 12:20:25 -0500

    Se separan las clases y se realizan pruebas

---
+++ BigLong/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <conio.h>
+#include <iomanip>  // std::setfill, std::setw
+#include "BigLong.cpp"
+
+using namespace std;
+
+//Biglong factorial(Biglong n)
+//{
+//	if (n == 1)return 1;
+//	return n * factorial(n - 1);
+//}
+
+Biglong factorial(Biglong n)
+{
+	Biglong aux=1, i;
+	for (i = 1; i <= n; ++i)
+	{
+		aux *= i;
+	}
+	return aux;
+}
+
+int main() {
+
+	//for( int x : squares )
+	//for (auto x : squares)
+
+	//list a;
+
+
+	//int a[] = { 1,2,3,4,5 };
+	//int b[] = { 0,0,0,0,0,0,0,0,0,0 };
+	cout << factorial("50000");
+
+	cout << "\n|||Fin||||" << endl;
+	_getch();
+	system("CLS");
+	//goto inicio;
+	return 0;
+}
